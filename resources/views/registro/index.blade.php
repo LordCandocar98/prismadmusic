81eec53342342ca0501d3b276d9a8ce217c7e4e0
JohanVacca <johan.vacca12@gmail.com>
2022-01-20 21:54:37 -0500

    MiddlewareAutenticado - Modalv1

--- resources/views/registro/index.blade.php
+++ resources/views/registro/index.blade.php
@@ -130,9 +130,33 @@
                     </div>
                     <div class="modal-footer">
                         <button type="submit" class="btn btn-primary">Aceptar</button>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalContratos">
+                            Términos y Condiciones
+                        </button>
                     </div>
                 </form>
             </div>
+        </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="modalContratos" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+            <div class="modal-body">
+            ...
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
         </div>
     </div>
 @endsection
