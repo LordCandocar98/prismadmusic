b22da6799a152b545b4967addd9f762d46278a57
CandidoM <stiivenmoreno@gmail.com>
2022-09-11 15:16:52 -0500

    Color corregidos de rojo a negro

--- resources/views/cancion/gestion/detalle.blade.php
+++ resources/views/cancion/gestion/detalle.blade.php
@@ -22,89 +22,89 @@
                         <input type="hidden" id="id_cancion" value="{{ $cancion->id }}">
                         <legend>Detalles de canción</legend>
                         <div class="panel-title col-sm-4">
-                            <h3><code>Título</code></h3>
+                            <h3><strong>Título</strong></h3>
                             {{ $cancion->titulo }}
                         </div>
                         <div class="panel-title col-sm-4">
-                            <h3><code>Versión / Subtítulo</code></h3>
+                            <h3><strong>Versión / Subtítulo</strong></h3>
                             {{ $cancion->version_subtitulo }}
                         </div>
                         <div class="panel-title col-sm-4">
-                            <h3><code>Tipo secundario</code></h3>
+                            <h3><strong>Tipo secundario</strong></h3>
                             {{ $cancion->tipo_secundario }}
                         </div>
                     </div>
                     <div class="form-group row panel-heading">
                         <div class="panel-title col-sm-4">
-                            <h3><code>Instrumental</code></h3>
+                            <h3><strong>Instrumental</strong></h3>
                             {{ $cancion->instrumental }}
                         </div>
                         <div class="panel-title col-sm-4">
-                            <h3><code>Autor</code></h3>
+                            <h3><strong>Autor</strong></h3>
                             {{ $cancion->autor }}
                         </div>
                         <div class="panel-title col-sm-4">
-                            <h3><code>Compositor</code></h3>
+                            <h3><strong>Compositor</strong></h3>
                             {{ $cancion->compositor }}
                         </div>
                         <legend></legend>
                     </div>
                     <div class="form-group row panel-heading">
                         <div class="panel-title col-sm-4">
-                            <h3><code>Arreglista</code></h3>
+                            <h3><strong>Arreglista</strong></h3>
                             {{ $cancion->arreglista }}
                         </div>
                         <div class="panel-title col-sm-4">
-                            <h3><code>Productor</code></h3>
+                            <h3><strong>Productor</strong></h3>
                             {{ $cancion->productor }}
                         </div>
                         <div class="panel-title col-sm-4">
-                            <h3><code>Pline</code></h3>
+                            <h3><strong>Pline</strong></h3>
                             {{ $cancion->pline }}
                         </div>
                         <legend></legend>
                     </div>
                     <div class="form-group row panel-heading">
                         <div class="panel-title col-sm-4">
-                            <h3><code>Año de producción</code></h3>
+                            <h3><strong>Año de producción</strong></h3>
                             {{ $cancion->annio_produccion }}
                         </div>
                         <div class="panel-title col-sm-4">
-                            <h3><code>Género</code></h3>
+                            <h3><strong>Género</strong></h3>
                             {{ $cancion->genero }}
                         </div>
                         <div class="panel-title col-sm-4">
-                            <h3><code>Subgénero</code></h3>
+                            <h3><strong>Subgénero</strong></h3>
                             {{ $cancion->subgenero }}
                         </div>
                         <legend></legend>
                     </div>
                     <div class="form-group row panel-heading">
                         <div class="panel-title col-sm-4">
-                            <h3><code>Género secundario</code></h3>
+                            <h3><strong>Género secundario</strong></h3>
                             {{ $cancion->genero_secundario }}
                         </div>
                         <div class="panel-title col-sm-4">
-                            <h3><code>Subgénero secundario</code></h3>
+                            <h3><strong>Subgénero secundario</strong></h3>
                             {{ $cancion->subgenero_secundario }}
                         </div>
                         <div class="panel-title col-sm-4">
-                            <h3><code>Letra chocante o vulgar</code></h3>
+                            <h3><strong>Letra chocante o vulgar</strong></h3>
                             {{ $cancion->letra_chocante_vulgar }}
                         </div>
                         <legend></legend>
                     </div>
                     <div class="form-group row panel-heading">
                         <div class="panel-title col-sm-4">
-                            <h3><code>Inicio de la previsualización</code></h3>
+                            <h3><strong>Inicio de la previsualización</strong></h3>
                             {{ $cancion->inicio_previsualizacion }}
                         </div>
                         <div class="panel-title col-sm-4">
-                            <h3><code>Idioma del título</code></h3>
+                            <h3><strong>Idioma del título</strong></h3>
                             {{ $cancion->idioma_titulo }}
                         </div>
                         <div class="panel-title col-sm-4">
-                            <h3><code>Fecha principal de salida</code></h3>
+                            <h3><strong>Fecha principal de salida</strong></h3>
                             {{ $cancion->fecha_principal_salida }}
                         </div>
                         <legend></legend>
