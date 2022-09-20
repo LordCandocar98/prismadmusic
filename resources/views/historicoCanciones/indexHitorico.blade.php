f6737168fa210f526b1e5b649021aea9f88202d0
Javier_Varon <jvaronbueno@gmail.com>
2022-09-19 22:28:15 -0500

    Corrección correo class

--- resources/views/historicoCanciones/indexHitorico.blade.php
+++ resources/views/historicoCanciones/indexHitorico.blade.php
@@ -27,7 +27,7 @@
                     </div>
                     <div class="form-group  col-md-6 ">
                         <label class="control-label" for="correo">Correo</label>
-                        <input type="text" name="correo" id="correo" class="valor form-control" step="any" value="{{ $user->email }}" readonly/>
+                        <input type="text" name="correo" id="correo" class="form-control" step="any" value="{{ $user->email }}" readonly/>
                     </div>
                 </fieldset>
             </div>
