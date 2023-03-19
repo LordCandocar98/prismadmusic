db3e7b24dc4b61571cd7aa80ac9e9989cbdf6faa
santiagoroncancio <sntgrncnc@gmail.com>
2023-03-18 19:34:10 -0500

    Cambio del color de la fuente

--- config/voyager.php
+++ config/voyager.php
@@ -195,6 +195,7 @@ return [
 
     // Here you can specify additional assets you would like to be included in the master.blade
     'additional_css' => [
+        'css/custom.css',
         'fa/css/font-awesome.min.css'
     ],
 
